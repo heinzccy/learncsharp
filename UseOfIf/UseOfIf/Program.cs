c4222cf3aa69842f31cb4c867bdb347c4fddd7b5
ccy <ccy@tywk.com>
2018-12-23 12:11:23 +0800

    Sync to Remote

---
+++ UseOfIf/UseOfIf/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseOfIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = Console.ReadLine();
+            int age = Convert.ToInt32(s1);
+            if (age>20)
+            {
+                
+            }
+        }
+    }
+}
